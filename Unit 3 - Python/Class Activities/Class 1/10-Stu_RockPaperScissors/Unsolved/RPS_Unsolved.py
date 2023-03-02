7c992f9a861c3d70a485eecae10ebc4924ec7aa4
pirodhimitri <piro_d@hotmail.com>
2023-03-01 22:35:30 -0500

    post python class 1 push

--- Unit 3 - Python/Class Activities/Class 1/10-Stu_RockPaperScissors/Unsolved/RPS_Unsolved.py
+++ Unit 3 - Python/Class Activities/Class 1/10-Stu_RockPaperScissors/Unsolved/RPS_Unsolved.py
@@ -14,3 +14,32 @@ computer_choice = random.choice(options)
 user_choice = input("Make your Choice: (r)ock, (p)aper, (s)cissors? ")
 
 # Run Conditionals
+if (user_choice == "r") and (computer_choice == 'p'): 
+    print("you chose rock, computer chose paper")
+    print("you lose!")
+elif (user_choice == "r") and (computer_choice == 's'): 
+    print("you chose rock, computer chose scissors")
+    print("you win!")
+elif (user_choice == "r") and (computer_choice == 'r'): 
+    print("you chose rock, computer chose rock")
+    print("you tie!")
+elif (user_choice == "p") and (computer_choice == 'p'): 
+    print("you chose paper, computer chose paper")
+    print("you tie!")
+elif (user_choice == "p") and (computer_choice == 's'): 
+    print("you chose paper, computer chose scissors")
+    print("you lose!")
+elif (user_choice == "p") and (computer_choice == 'r'): 
+    print("you chose paper, computer chose rock")
+    print("you win!")
+elif (user_choice == "s") and (computer_choice == 'p'): 
+    print("you chose scissors, computer chose paper")
+    print("you win!")
+elif (user_choice == "s") and (computer_choice == 's'): 
+    print("you chose scissors, computer chose scissors")
+    print("you tie!")
+elif (user_choice == "s") and (computer_choice == 'r'): 
+    print("you chose scissors, computer chose rock")
+    print("you lose!")
+else: 
+    print("You input somethign other than r, p or s.")
